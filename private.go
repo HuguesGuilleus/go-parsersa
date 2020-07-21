68b0e5a3193bffa7addb033f94903de934dce34e
hugues <ghugues@netc.fr>
2020-07-21 16:00:50 +0200

    private.go: Add GenPrivKey()

--- private.go
+++ private.go
@@ -5,12 +5,47 @@
 package parsersa
 
 import (
+	"crypto/rand"
 	"crypto/rsa"
 	"crypto/x509"
 	"encoding/pem"
 	"errors"
+	"fmt"
 	"io/ioutil"
+	"os"
 )
+
+// Try to read the file key or generate it if the file don't exist.
+func GenPrivKey(file string, size int) (*rsa.PrivateKey, error) {
+	// Read the file
+	if k, err := PrivFile(file); err == nil {
+		if s := k.N.BitLen(); s < size {
+			return nil, fmt.Errorf("The key have a too short len: %d, want %d", s, size)
+		}
+		return k, nil
+	} else if !os.IsNotExist(err) {
+		return nil, err
+	}
+
+	// Generate the key
+	k, err := rsa.GenerateKey(rand.Reader, size)
+	if err != nil {
+		return nil, err
+	}
+
+	// Save the new key
+	f, err := os.OpenFile(file, os.O_WRONLY|os.O_CREATE, 0664)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+	pem.Encode(f, &pem.Block{
+		Type:  "RSA PRIVATE KEY",
+		Bytes: x509.MarshalPKCS1PrivateKey(k),
+	})
+
+	return k, nil
+}
 
 // Parse a private key from a file.
 func PrivFile(file string) (*rsa.PrivateKey, error) {
